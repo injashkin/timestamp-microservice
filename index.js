dbc3760a59f06a1fa9c727df710f08c89df810b6
Игорь Яшкин <injashkin@gmail.com>
2020-01-23 23:34:15 +0300

    edit

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 const express = require("express");
 const app = express();
+var cors = require('cors');
+app.use(cors()); 
+
+//Если строка даты пуста, то выводится текущее время
+app.get("/api/timestamp/", (req, res) => {
+  const timeInMs = Date.now();
+  const timeUTC = new Date().toUTCString();
+  res.json({
+    unix: timeInMs, utc: timeUTC
+  });
+});
 
 app.get("/api/timestamp/:date", (req, res) => {
 
@@ -15,15 +26,6 @@ app.get("/api/timestamp/:date", (req, res) => {
   const time = new Date(date).toUTCString();
   res.json({
     unix: Date.parse(time), utc: time
-  });
-});
-
-//Если строка даты пуста, то выводится текущее время
-app.get("/api/timestamp/", (req, res) => {
-  const timeInMs = Date.now();
-  const timeUTC = new Date().toUTCString();
-  res.json({
-    unix: timeInMs, utc: timeUTC
   });
 });
 

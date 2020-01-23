f4154751c198c25b12b88a1678a8f7f76d72104c
Игорь Яшкин <injashkin@gmail.com>
2020-01-24 00:13:02 +0300

    edit

--- index.js
+++ index.js
@@ -1,31 +1,34 @@
 const express = require("express");
 const app = express();
-var cors = require('cors');
-app.use(cors()); 
+var cors = require("cors");
+app.use(cors());
+
+app.get("/:date", (req, res) => {
+  const { date } = req.params;
+  console.log(date)
+
+  if (Number(date)) {
+    const timeInMs = Number(date);
+    const time = new Date(timeInMs).toUTCString();
+    res.json({
+      unix: timeInMs,
+      utc: time
+    });
+  }
+  const time = new Date(date).toUTCString();
+  res.json({
+    unix: Date.parse(time),
+    utc: time
+  });
+});
 
 //Если строка даты пуста, то выводится текущее время
 app.get("/", (req, res) => {
   const timeInMs = Date.now();
   const timeUTC = new Date().toUTCString();
   res.json({
-    unix: timeInMs, utc: timeUTC
-  });
-});
-
-app.get("/:date", (req, res) => {
-
-  let date = req.params.date;
-
-  if (Number(date)) {
-    const timeInMs = Number(date);
-    const time = new Date(timeInMs).toUTCString();
-    res.json({
-      unix: timeInMs, utc: time
-    });
-  }
-  const time = new Date(date).toUTCString();
-  res.json({
-    unix: Date.parse(time), utc: time
+    unix: timeInMs,
+    utc: timeUTC
   });
 });
 
